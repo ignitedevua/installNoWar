9fe6cfdd1b7425aa7f499a7c1d8c3a6a492d74fb
ignitedevua <ignitedevua@gmail.com>
2022-03-09 16:51:31 +0200

    py ver setup

---
+++ setup_py.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+main() {
+    threads="${1:-3}"
+    echo "Setup, threads: ${threads}" 
+
+    echo "Check python3 ..."
+    which python3 || exit 1
+    echo "OK"
+
+    echo "Cloning repo ..."
+    git clone -b "feaure/min-logs" "https://gitlab.com/a_gonda/nowarddos.git" || exit 1
+    echo "OK"
+
+
+    python -m venv "nowarddos"
+    cd "nowarddos"
+
+    echo "Install deps ... "
+    pip3 install -r requirements.txt  &> /dev/null
+    echo "OK"
+
+    echo "Start script ... "
+    python3 "main.py" ${threads}
+}
+
+main "${@}"
